58ae1ca13c8356b40332d50b436909037a3bcd37
Dimmsum <153735532+Dimmsum@users.noreply.github.com>
2025-04-23 21:56:31 +0000

    About me page

--- spiritual-vitamins/src/App.jsx
+++ spiritual-vitamins/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login'
 import Posts from './pages/Posts'
 import ManageVitamins from './pages/ManagePosts'
 import PostDetails from './pages/PostDetails';
-
+import AboutPage from './pages/About';
 
 
 
@@ -20,6 +20,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/vitamins" element={<Posts />} />
           <Route path="/manage-vitamins" element={<ManageVitamins />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/vitamins/:id/:slug" element={<PostDetails />} />
           {/* Add more routes as needed */}
         </Routes>
